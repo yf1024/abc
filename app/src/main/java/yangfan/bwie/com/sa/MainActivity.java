2771c8cb5a6bc93372a65fc669236e87b3f8bcda
Author Name <Author Email>
2018-04-10 11:51:23 +0800

    *****

---
+++ app/src/main/java/yangfan/bwie/com/sa/MainActivity.java
@@ -0,0 +1,13 @@
+package yangfan.bwie.com.sa;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
